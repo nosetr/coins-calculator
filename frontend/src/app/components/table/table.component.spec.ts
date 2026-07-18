b3e9960339647a029bc04da4ea61dfc986a2aadc
Nikolay Osetrov <osetrov.n@gmail.com>
2026-07-18 22:41:59 +0200

    feat: front app test init

--- frontend/src/app/components/table/table.component.spec.ts
+++ frontend/src/app/components/table/table.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TableComponent } from './table.component';
+import { CalculateTypeEnum } from '../../app';
 
 describe('TableComponent', () => {
   let component: TableComponent;
@@ -9,12 +9,21 @@ describe('TableComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TableComponent]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TableComponent);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+
+    fixture.componentRef.setInput('result', {
+      newAmount: 100,
+      oldAmount: null,
+      newDenomination: {},
+      difference: {}
+    });
+
+    fixture.componentRef.setInput('type', CalculateTypeEnum.BACK);
+
+    fixture.detectChanges();
   });
 
   it('should create', () => {
